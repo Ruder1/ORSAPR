db79dfb01864d00f60a1fa4d525d1cfc690d8116
Ruder1 <cpc15@mail.ru>
2021-11-14 19:08:42 +0700

    Создан проект Hangers Plugin

---
+++ Src/MainForm/HangersPlugin/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace HangersPlugin
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainTextLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MainTextLabel
+            // 
+            this.MainTextLabel.AutoSize = true;
+            this.MainTextLabel.Location = new System.Drawing.Point(12, 51);
+            this.MainTextLabel.Name = "MainTextLabel";
+            this.MainTextLabel.Size = new System.Drawing.Size(189, 13);
+            this.MainTextLabel.TabIndex = 0;
+            this.MainTextLabel.Text = "Write the input data for Hangers below";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 524);
+            this.Controls.Add(this.MainTextLabel);
+            this.Name = "MainForm";
+            this.Text = "Hangers For Clothes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainTextLabel;
+    }
+}
+
